1587f0884aefb844e5f546a0be408f88e119f7e1
Jaki Widby <jaki.widby@gmail.com>
2019-12-16 16:42:53 -0800

    create array containing all list items use .sort to sort the items alphabetically.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("form#list").submit(function(event) {
+    event.preventDefault();
+
+    var listItem1 = $(one).val();
+    var listItem2 = $(two).val();
+    var listItem3 = $(three).val();
+    var listItems = [listItem1, listItem2, listItem3];
+    listItems.sort();
+    alert(listItems);
+
+    $(".container").hide();
+  });
+});
